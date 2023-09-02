573cb93ab109e1c69ac8a0fe674f1ef8664b4dff
Doplax <doplax@gmail.com>
2023-09-02 15:23:45 +0200

    Exercise 1 completed

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+// ejercicio1.js
+
+const estudianteBootcamp = {
+  nombre: 'Pol',
+  apellidos: 'Valle Montes',
+  temasConocidos: ['HTML', 'CSS', 'JavaScript'],
+  busquedaActiva: true,
+  redesSociales: [
+    {
+      nombre: 'Github',
+      link: 'https://github.com/Doplax'
+    }
+  ]
+};
+
+console.log(estudianteBootcamp);
